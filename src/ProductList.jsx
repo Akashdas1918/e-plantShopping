dce2560028e70c68478d446521acf4368c86c7ed
Akash das <akashdas1918@gmail.com>
2025-05-29 20:05:03 +0530

    Implement cart functionality with Redux and add CartItem component

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice'; // Adjust path as needed
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-    const plantsArray = [
+   const dispatch = useDispatch();
+ const plantsArray = [
         {
             category: "Air Purifying Plants",
             plants: [
